dc7c20715900dcdab2216c04337046649539e23c
[dyad] <ravmkt1979@gmail.com>
2026-07-18 09:54:14 -0300

    Updated public/widget.js

--- public/widget.js
+++ public/widget.js
@@ -1,5 +1,5 @@
 (function () {
-    var WIDGET_VERSION = '2026.07.18-02';
+    var WIDGET_VERSION = '2026.07.18-03';
 
   console.info(
     '%cVidlytics Widget carregado — versão ' + WIDGET_VERSION,
