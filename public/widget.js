9a88d9e38e07e94c66767d814aadd67546f3491a
Rodrigo <ravmkt1979@gmail.com>
2026-07-20 17:11:06 -0300

    Atualiza widget com versão local

--- public/widget.js
+++ public/widget.js
@@ -1,5 +1,5 @@
 (function () {
-    var WIDGET_VERSION = '2026.07.20-08';
+    var WIDGET_VERSION = '2026.07.20-09';
 
   console.info(
     '%cVidlytics Widget carregado — versão ' + WIDGET_VERSION,
