0e6cf79b772dd3e5d3c056eafd569d1ab274f396
[dyad] <ravmkt1979@gmail.com>
2026-07-21 08:13:10 -0300

    [dyad] Show insertCarouselHostBySelector code

--- public/widget.js
+++ public/widget.js
@@ -1,5 +1,5 @@
 (function () {
-    var WIDGET_VERSION = '2026.07.20-20';
+    var WIDGET_VERSION = '2026.07.21-00';
 
   console.info(
     '%cVidlytics Widget carregado — versão ' + WIDGET_VERSION,
