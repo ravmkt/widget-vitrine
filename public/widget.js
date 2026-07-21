3af10c07242cbb56a551987cc6924dedc54a6c76
[dyad] <ravmkt1979@gmail.com>
2026-07-21 09:27:12 -0300

    Updated public/widget.js

--- public/widget.js
+++ public/widget.js
@@ -1,5 +1,5 @@
 (function () {
-    var WIDGET_VERSION = '2026.07.21-04';
+    var WIDGET_VERSION = '2026.07.21-05';
 
   console.info(
     '%cVidlytics Widget carregado — versão ' + WIDGET_VERSION,
