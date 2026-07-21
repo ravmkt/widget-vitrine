0d801d0b4540663d50f677ad1dc6f4e3d7e58dba
[dyad] <ravmkt1979@gmail.com>
2026-07-20 21:35:34 -0300

    [dyad] Localizar e corrigir chaves desbalanceadas em public/widget.js

--- public/widget.js
+++ public/widget.js
@@ -1,5 +1,5 @@
 (function () {
-    var WIDGET_VERSION = '2026.07.20-16';
+    var WIDGET_VERSION = '2026.07.20-17';
 
   console.info(
     '%cVidlytics Widget carregado — versão ' + WIDGET_VERSION,
