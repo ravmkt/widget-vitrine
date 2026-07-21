ea29fd84ff6813f5a6a58318a632bfc4ff55478a
[dyad] <ravmkt1979@gmail.com>
2026-07-21 15:25:51 -0300

    Updated public/widget.js

--- public/widget.js
+++ public/widget.js
@@ -1,5 +1,5 @@
 (function () {
-    var WIDGET_VERSION = '2026.07.21-07';
+    var WIDGET_VERSION = '2026.07.21-08';
 
   console.info(
     '%cVidlytics Widget carregado — versão ' + WIDGET_VERSION,
