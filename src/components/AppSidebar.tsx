e3ccf739a623a6a6175129111dce6b92920b2b3c
[dyad] <ravmkt1979@gmail.com>
2026-07-22 16:07:51 -0300

    [dyad] Restaurar logo original e aumentar tamanho no menu lateral

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -84,7 +84,8 @@ export function AppSidebar() {
 
       <SidebarHeader className="p-6">
         <div className="flex items-center gap-3">
-          <img src="/assets/vidlytics-logo-wide.png" alt="Vidlytics" className="h-10 w-auto" />
+          <img src="/assets/vidlytics-logo-wide.png" alt="Vidlytics" className="h-12 w-auto max-w-[220px]" />
+
         </div>
       </SidebarHeader>
 
