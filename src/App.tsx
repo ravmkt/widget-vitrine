c8c483d249491f2b1fec41dc45a50cbe3b49d6b2
[dyad] <ravmkt1979@gmail.com>
2026-07-26 14:58:38 -0300

    Updated src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,6 @@ import MedidasPage from "@/pages/MedidasPage";
 import AppearancePage from "@/pages/AppearancePage";
 import CommentsPage from "@/pages/CommentsPage";
 import StoryNewPage from "@/pages/StoryNewPage";
-import StoryEditPage from "@/pages/StoryEditPage";
 
 
 
@@ -127,7 +126,7 @@ function App() {
 
           {/* Rotas protegidas (só acessa se estiver logado) */}
           <Route path="/dashboard" element={<ProtectedRoute><AppLayout><DashboardPage /></AppLayout></ProtectedRoute>} />
-<Route path="/stories/new" element={<ProtectedRoute><AppLayout><StoryNewPage /></AppLayout></ProtectedRoute>} />
+          <Route path="/stories/new" element={<ProtectedRoute><AppLayout><StoryNewPage /></AppLayout></ProtectedRoute>} />
           <Route path="/stories" element={<ProtectedRoute><AppLayout><StoriesPage /></AppLayout></ProtectedRoute>} />
           <Route path="/gallery" element={<ProtectedRoute><AppLayout><VideoGalleryPage /></AppLayout></ProtectedRoute>} />
           <Route path="/videos/new" element={<ProtectedRoute><AppLayout><VideoEditPage /></AppLayout></ProtectedRoute>} />
