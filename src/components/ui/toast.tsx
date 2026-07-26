ad8dca14bfe964b3901e1f9148b04c09a3e0b584
[dyad] <ravmkt1979@gmail.com>
2026-07-26 07:48:57 -0300

    Updated src/components/ui/toast.tsx

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -13,10 +13,10 @@ const ToastViewport = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <ToastPrimitives.Viewport
     ref={ref}
-    className={cn(
-      "fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]",
-      className,
-    )}
+className={cn(
+  "fixed top-0 left-1/2 -translate-x-1/2 z-[100] flex max-h-screen w-full flex-col items-center p-4 sm:top-0 sm:flex-col md:max-w-[420px]",
+  className,
+)}
     {...props}
   />
 ));
