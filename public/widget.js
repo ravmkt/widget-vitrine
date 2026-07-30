e164485b1ae45b7824cc30bf4dbc9c4a98861570
[dyad] <ravmkt1979@gmail.com>
2026-07-30 15:14:57 -0300

    Updated public/widget.js

--- public/widget.js
+++ public/widget.js
@@ -2782,7 +2782,7 @@ window.__vidlytics_debug_products = readProductsData;
       }
 
 storeWhatsappNumber = storeSettings.whatsapp_number || storeSettings.whatsappNumber || '';
-storeWhatsappNumber = storeSettings.whatsapp_number || storeSettings.whatsappNumber || '';
+storeWhatsappMessage = storeSettings.whatsapp_message_template || storeSettings.whatsapp_message || storeSettings.whatsappMessage || '';
 
       currentAppearance = appearance;
 
