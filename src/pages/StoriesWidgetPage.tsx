22f110edb593f97b6b5253f001fc890728b9728b
[dyad] <ravmkt1979@gmail.com>
2026-07-30 10:42:39 -0300

    Updated src/pages/StoriesWidgetPage.tsx

--- src/pages/StoriesWidgetPage.tsx
+++ src/pages/StoriesWidgetPage.tsx
@@ -551,11 +551,13 @@ const SocialActionButtons: FC<{
       className="flex items-center justify-center rounded-full p-3 text-white backdrop-blur-md transition hover:brightness-110"
       style={btnStyle}
     >
-      <MessageCircle
-        size={20}
-        fill={commentCount > 0 ? 'white' : 'none'}
-        color="white"
-      />
+      {commentCount > 0 ? (
+        <svg width="20" height="20" viewBox="0 0 24 24" fill="white" stroke="white" strokeWidth="1.5" xmlns="http://www.w3.org/2000/svg">
+          <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z" />
+        </svg>
+      ) : (
+        <MessageCircle size={20} color="white" />
+      )}
     </div>
     <span className="text-[10px] font-black text-white leading-none">
       {commentCount}
