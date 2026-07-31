ac8708c4489e52c4d3ee8cedaded5149e35412c9
[dyad] <ravmkt1979@gmail.com>
2026-07-31 16:20:13 -0300

    Updated public/widget.js

--- public/widget.js
+++ public/widget.js
@@ -92,7 +92,7 @@
     carousel_show_play_button: true,
     carousel_auto_center: false,
     grid_shape: 'portrait',
-    grid_size: '30',
+    grid_size: '200',
     grid_columns: '4',
     grid_rows: '1',
     grid_spacing: '16',
