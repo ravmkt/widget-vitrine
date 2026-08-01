1f0ef692630334f9d770d2922ad4da13bc795eb9
[dyad] <ravmkt1979@gmail.com>
2026-08-01 09:03:46 -0300

    Updated src/pages/AppearancePage.tsx

--- src/pages/AppearancePage.tsx
+++ src/pages/AppearancePage.tsx
@@ -1817,7 +1817,7 @@ const ModalPreview = ({
                         className="flex-1 rounded-lg px-2 py-1.5 text-[10px] font-black text-white"
                         style={{ backgroundColor: '#25D366' }}
                       >
-                        WhatsApp
+                        Comprar pelo WhatsApp
                       </button>
                     )}
                   </div>
