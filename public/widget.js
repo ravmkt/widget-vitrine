bbeebd7a6dd91c76aedaa290257fbe9f89fbeadc
[dyad] <ravmkt1979@gmail.com>
2026-08-08 12:03:23 -0300

    Updated public/widget.js

--- public/widget.js
+++ public/widget.js
@@ -3229,7 +3229,7 @@ wrapper.style.cssText = [
   wrapper.appendChild(track);
   container.appendChild(wrapper);
 
-  
+  enableDragScroll(wrapper);
 
   console.log('[Vidlytics] Carrossel renderizado com ' + allVideos.length + ' video(s).');
 }
