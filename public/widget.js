1804f46de510804e6090cceced2b2c5c2b42d360
[dyad] <ravmkt1979@gmail.com>
2026-08-11 13:37:38 -0300

    Updated public/widget.js

--- public/widget.js
+++ public/widget.js
@@ -569,7 +569,14 @@ function normalizeFloatingPosition(pos) {
   return 'bottom-right';
 }
 
-
+function normalizeFloatingShape(value) {
+    var key = normalizeKey(value);
+    if (key === 'square' || key === 'quadrado') return 'square';
+    if (key === 'portrait' || key === 'retrato' || key === '9-16') return 'portrait';
+    if (key === 'landscape' || key === 'paisagem' || key === '16-9' || key === '16_9') return 'landscape';
+    if (key === 'circle' || key === 'circulo' || key === 'redondo') return 'circle';
+    return DEFAULT_APPEARANCE.floating_shape;
+  }
 
 function getFloatingConfig(appearance) {
   appearance = normalizeAppearanceItem(appearance || {});
