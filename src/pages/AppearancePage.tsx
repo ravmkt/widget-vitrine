62e4f79f0ee20d245dc0de3f679969b2d421e919
[dyad] <ravmkt1979@gmail.com>
2026-08-11 13:13:25 -0300

    Updated src/pages/AppearancePage.tsx

--- src/pages/AppearancePage.tsx
+++ src/pages/AppearancePage.tsx
@@ -1847,23 +1847,7 @@ const ModalPreview = ({
                 >
                   R$ 149,95
                 </p>
-                <div className="mt-1 flex gap-1">
-                  {m.show_product_button && (
-                    <button
-                      type="button"
-                      className="flex-1 rounded px-1.5 py-1 text-[8px] font-black text-white"
-                      style={{ backgroundColor: colors.button }}
-                    >
-                      Ver produto
-                    </button>
-                  )}
-                  <button
-                    type="button"
-                    className="flex-1 rounded px-1.5 py-1 text-[8px] font-black text-white bg-[#25D366]"
-                  >
-                    WhatsApp
-                  </button>
-                </div>
+
               </div>
             </div>
           </div>
