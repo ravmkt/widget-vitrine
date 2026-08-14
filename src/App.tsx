1ac31c47cbb6b453a406ba6ff6b054c68d9bef03
[dyad] <ravmkt1979@gmail.com>
2026-08-14 18:04:37 -0300

    Updated src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,7 @@ import AppearancePage from "@/pages/AppearancePage";
 import CommentsPage from "@/pages/CommentsPage";
 import StoragePage from "@/pages/StoragePage";
 import { BillingPage } from "@/pages/BillingPage";
+import { PlansPage } from "@/pages/PlansPage";
 import InstagramCallback from "@/pages/auth/InstagramCallback";
 import StoryPreviewPage from "@/pages/StoryPreviewPage";
 
