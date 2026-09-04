83a4ff1c9a6ec630fa7bbabd4a71a1d45319c8b4
[dyad] <ravmkt1979@gmail.com>
2026-09-04 16:43:56 -0300

    Updated public/widget.js

--- public/widget.js
+++ public/widget.js
@@ -4151,19 +4151,6 @@ sendAnalyticsEvent('product_view', item.id || null, pData.id || null, { storyId:
       track.appendChild(card);
     });
 
-    // --- INÍCIO: CLONES PARA LOOP INFINITO ---
-    var itemsArray = Array.from(track.children);
-    if (itemsArray.length > 0) {
-      var cloneLast = itemsArray[itemsArray.length - 1].cloneNode(true);
-      cloneLast.classList.add('vidlytics-clone', 'clone-left');
-      track.insertBefore(cloneLast, track.firstChild);
-
-      var cloneFirst = itemsArray[0].cloneNode(true);
-      cloneFirst.classList.add('vidlytics-clone', 'clone-right');
-      track.appendChild(cloneFirst);
-    }
-    // --- FIM: CLONES PARA LOOP INFINITO ---
-
     viewport.appendChild(track);
     wrapper.appendChild(viewport);
     container.appendChild(wrapper);
